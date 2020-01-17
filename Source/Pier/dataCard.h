137f068b552203a20f785cff56d0a38ff078e519
Danilo S. Tuzita <danilo_st@hotmail.com>
2020-01-16 22:11:12 -0300

    Contents

---
+++ Source/Pier/dataCard.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PIER_API dataCard
+{
+public:
+	dataCard();
+	~dataCard();
+};
